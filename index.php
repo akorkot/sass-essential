58878063c68494e600efbc8a8a9d4f537529a4a0
vagrant <vagrant@drupalvm.dev>
2017-03-24 22:25:11 +0000

    Tutorial sass

---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Sass tutorial</title>
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link href="css/style.css" rel="stylesheet">
+</head>
+    <body>
+        <h1>Hello, world!</h1>
+        <br /><br /><br />
+        <div class="par1">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam vehicula mi ac justo congue, in mollis ligula
+            euismod. Etiam sit amet sodales sem. Vivamus elementum condimentum ultricies. Fusce venenatis dapibus urna, vitae
+            feugiat metus bibendum in. Nullam quam sapien, lobortis ac erat vitae, auctor bibendum mi. Sed at erat at urna
+            faucibus ultricies a ac ligula. Nulla facilisi. Nam et ultricies dui. Ut feugiat molestie venenatis. Nulla vitae
+            metus scelerisque, luctus mi eu, condimentum libero. Vestibulum dignissim gravida pellentesque. Donec suscipit
+            scelerisque massa, ac iaculis quam viverra ac.
+        </div>
+        <div class="par2">
+            Ut vitae egestas elit. Duis ante turpis, imperdiet condimentum varius ac, tincidunt ultricies libero. Interdum et
+            malesuada fames ac ante ipsum primis in faucibus. Pellentesque faucibus ac nulla nec tincidunt. Sed id finibus
+            sapien. Maecenas placerat consequat cursus. Pellentesque sodales sagittis purus, quis vehicula lacus suscipit vitae.
+            Etiam vitae urna non dolor congue fringilla id id turpis. Ut nec porttitor nisi, at maximus magna. Pellentesque
+            malesuada nibh dolor, et venenatis nisl condimentum sed. Sed fringilla dignissim arcu. Nulla vel ligula convallis,
+            dictum urna quis, finibus dolor. Aliquam erat volutpat.
+        </div>
+        <br /><br /><br />
+        <nav class="navbar navbar-default">
+            <div class="container-fluid">
+                <ul class="nav navbar-nav">
+                    <li class="active"><a href="#">Home</a></li>
+                    <li><a href="#">Page 1</a></li>
+                    <li><a href="#">Page 2</a></li>
+                    <li><a href="#">Page 3</a></li>
+                </ul>
+            </div>
+        </nav>
+        <br /><br /><br />
+        <div class="box">
+            Ut vitae egestas elit. Duis ante turpis, imperdiet condimentum varius ac, tincidunt ultricies libero. Interdum et.
+        </div>
+        <br /><br /><br />
+        <div class="message error">
+            Ut vitae egestas elit. Duis ante turpis, imperdiet condimentum varius ac, tincidunt ultricies libero. Interdum et.
+        </div>
+        <div class="message success">
+            Ut vitae egestas elit. Duis ante turpis, imperdiet condimentum varius ac, tincidunt ultricies libero. Interdum et.
+        </div>
+        <div class="message warning">
+            Ut vitae egestas elit. Duis ante turpis, imperdiet condimentum varius ac, tincidunt ultricies libero. Interdum et.
+        </div>
+    </body>
+</html>
